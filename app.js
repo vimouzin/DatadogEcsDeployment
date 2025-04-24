19639c30b875c0c81334315991112b9157e567c1
vimouzin <vitor.mouzinho@datadoghq.com>
2025-04-23 22:28:09 -0600

    update

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
-// Load Datadog tracing before any other modules
-const tracer = require('dd-trace').init({
-    service: process.env.DD_SERVICE || 'default-service-name',
-    env: process.env.DD_ENV || 'development',
-    version: process.env.DD_VERSION || '1.0.0',
-    logInjection: process.env.DD_LOGS_INJECTION === 'true',
-    analytics: true, // Optional: Enable trace analytics
-  });
-  
   
   // Import the necessary libraries
-  const express = require('express');
-  const winston = require('winston');
+  const express = require('express');  const winston = require('winston');
   
   // Initialize the application
   const app = express();
@@ -34,22 +24,12 @@ const tracer = require('dd-trace').init({
   app.get('/', (req, res) => {
     logger.info('Root route accessed', { route: '/', method: 'GET' });
     logger.info("test vitor");
-  
-    // Simulating a traced operation
-    tracer.trace('sample.operation', () => {
-      // Perform some operation here
-      logger.info('Operation within trace', { operation: 'sample.operation' });
-    });
-  
     res.send('Hello from Datadog-traced Node app!');
   });
   
   // Route to simulate an error log and trace
   app.get('/error', (req, res) => {
     logger.error('Error route accessed', { route: '/error', method: 'GET' });
-    tracer.trace('error.operation', () => {
-      throw new Error('Simulated error for tracing');
-    });
     res.status(500).send('Error route triggered');
   });
   
---
+++ dockerfile
@@ -0,0 +1,20 @@
+# Use the node:14-buster-slim image
+FROM --platform=linux/amd64 node:14-buster-slim
+
+
+# Create app directory
+WORKDIR /usr/src/app
+
+# Copy package.json and package-lock.json to install dependencies
+COPY package*.json ./
+
+# Install app dependencies
+RUN npm install
+
+# Copy application code to the container
+COPY . .
+
+# Expose the port your application runs on
+EXPOSE 3000
+
+CMD ["node", "app.js"]
